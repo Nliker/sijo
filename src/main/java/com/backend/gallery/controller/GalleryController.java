2d955fcf6f070da2937462544d52a4b222a34d29
codakcodak <tjwjdgus83@naver.com>
2023-12-21 14:31:27 +0900

    add import

--- src/main/java/com/backend/gallery/controller/GalleryController.java
+++ src/main/java/com/backend/gallery/controller/GalleryController.java
@@ -1,5 +1,6 @@
 package com.backend.gallery.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.gallery.model.GalleryResponseDto;
+import com.backend.gallery.model.service.GalleryService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +25,7 @@ public class GalleryController {
 	private final GalleryService galleryService;
 	
 	@GetMapping("/{type}")
-	public ResponseEntity<Map<String,Integer>> gallerys(@PathVariable String type,@RequestParam Map<String, String> map){
+	public ResponseEntity<Map<String,List<GalleryResponseDto>>> gallerys(@PathVariable String type,@RequestParam Map<String, Integer> map) throws Exception{
 		log.debug("Get gallerys");
 		Map<String,List<GalleryResponseDto>> result=new HashMap<>();
 		result.put("galleryList",galleryService.getGalleryList(type,map));
