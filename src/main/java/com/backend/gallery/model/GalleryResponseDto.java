a45c75b05b9fcd5ff0f14110638c3bcde3386b78
codakcodak <tjwjdgus83@naver.com>
2023-12-21 14:40:12 +0900

    add column galleryTitle and galleryRegTime

--- src/main/java/com/backend/gallery/model/GalleryResponseDto.java
+++ src/main/java/com/backend/gallery/model/GalleryResponseDto.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 @Getter @Setter
 public class GalleryResponseDto {
 	private int galleryNo;
+	private String galleryTitle;
+	private String galleryRegTime;
 	private String thumbnailUri;
-	
 	
 	@Override
 	public String toString() {
-		return "GalleryResponseDto [galleryNo=" + galleryNo + ", thumbnailUri=" + thumbnailUri + "]";
+		return "GalleryResponseDto [galleryNo=" + galleryNo + ", galleryTitle=" + galleryTitle + ", galleryRegTime="
+				+ galleryRegTime + ", thumbnailUri=" + thumbnailUri + "]";
 	}
 }
